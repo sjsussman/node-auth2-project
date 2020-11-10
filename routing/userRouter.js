88a8e0b9fcc01bc597eddfad103c9c416da59ccb
Steven Sussman <sjsussman@gmail.com>
2020-11-10 15:10:41 -0500

    mvp

--- routing/userRouter.js
+++ routing/userRouter.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const router = express.Router();
 const bcrypt = require('bcryptjs') // bcrypt to hash user password on requests
-const secure = require('../authorization-MW.js')
-
+const jwt = require('jsonwebtoken') // for token creation / managment
 const Users = require('./userModel');
+const { jwtSecret } = require('../data/secrets.js');
+const accessGuard = require('../restrictionMW')
 
 router.get('/', (req, res) => {
     res.json({ api : 'running'})
 })
 
-router.get('/users', secure, (req, res) => {
+router.get('/users', accessGuard, (req, res) => {
     Users.find()
     .then(users => {
         res.status(200).json(users)
@@ -17,38 +18,68 @@ router.get('/users', secure, (req, res) => {
     .catch(err => res.send(err))
 })
 
-router.post('/register', async (req, res) => {
-    try{
-        const { username, password } = req.body
-        const hash = bcrypt.hashSync(password, 10)
-        const user = { username, password: hash }
-        const addedUser = await Users.add(user)
-        res.json(addedUser)
-    } catch(err) {
-        res.status(500).json({
-            message: err.message
+router.post('/register', (req, res) => {
+    const credentials = req.body
+    if(credentials) {
+        const hashCycles = process.env.HASH_CYCLES || 10;
+        const hash = bcrypt.hashSync(credentials.password, hashCycles)
+        credentials.password = hash;
+        Users.add(credentials)
+            .then(user => {
+                res.status(201).json({
+                    data: user
+                })
+            })
+            .catch(error => {
+                res.json(500).json({
+                    message: error.message
+                })
+            })
+    } else {
+        res.status(400).json({
+            message: "please provide a username and password that meets our guidelines"
         })
     }
 })
 
-router.post('/login', async (req, res) => {
-    try {
-        const [user] = await Users.findBy({ username: req.body.username })
-        if(user && bcrypt.compareSync(req.body.password, user.password)){
-            req.session.user = user
-            res.json({
-                message: `Welcome back, ${user.username}`
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if({ username, password }) {
+        Users.findBy({ username: username })
+        .then(([user]) => {
+            if(user && bcrypt.compareSync(password, user.password)) {
+                const token = createToken(user)
+                res.status(200).json({
+                    message: 'You have successfully logged in', token
+                })
+            } else {
+                res.status(401).json({
+                    message: 'Invalid Credentials'
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: error.message
             })
-        } else {
-            res.status(401).json({
-                message: 'You shall not pass!'
-            })
-        }
-    } catch (err) {
-        res.status(500).json({
-            message: err.message
+        })
+    } else {
+        res.status(400).json({
+            message: ' please provide an exisiting username and password combination'
         })
     }
 })
+
+function createToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+    }
+    const options = {
+        expiresIn: '60 seconds'
+    }
+    return jwt.sign(payload, jwtSecret, options)
+}
 
 module.exports = router;
